5ae34de3a99e4b778d7d7bad3930a2014fed5644
Quang <quangmacbook@Quangs-MacBook-Air.local>
2025-06-09 12:04:54 +0200

    Configuration that contains the API keys

---
+++ configurations_template.js
@@ -0,0 +1,12 @@
+//OpenAI
+const OPENAI_TOKEN = ''; //your OpenAI token here
+
+//Telegram
+const TELEGRAM_TOKEN = '';//your Telegram bot token here
+const TELEGRAM_API_URL = ``;//your Telegram API URL here
+const CHAT_ID = '';//your Telegram chat ID here
+const THREAD_ID = '';//your Telegram thread ID here, if any
+const BOT_USERNAME = '';//your Telegram bot username here
+
+//GSheets
+const SPREADSHEET_ID = '';//your Google Spreadsheet ID here
